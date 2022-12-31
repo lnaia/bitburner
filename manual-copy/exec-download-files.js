8abb526bd98196ca9fa1174d48ecc45052ee0e7a
Luis Naia <lnaia@users.noreply.github.com>
2022-12-31 13:34:15 +0000

    update deploy files

--- manual-copy/exec-download-files.js
+++ manual-copy/exec-download-files.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  const scriptArgs = ['USER', 'GIST_ID'];
+  const scriptArgs = ['lnaia', '2ca322ffbde25a80c39afb0c1e5cf731'];
   const files = ['lib.js', 'exec-coordinator.js'];
 
   files.forEach(file => {
