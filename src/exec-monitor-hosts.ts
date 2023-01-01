bde8892e12713a29a79430a65c8a8ae451059491
Luis Naia <lnaia@users.noreply.github.com>
2023-01-01 21:09:18 +0000

    Update exec-monitor-hosts.ts

--- src/exec-monitor-hosts.ts
+++ src/exec-monitor-hosts.ts
@@ -6,8 +6,22 @@ import type {NS} from './NetscriptDefinitions';
 export async function main(ns: NS) {
   ns.disableLog('ALL');
   ns.tail();
+
+  const maxHosts = +ns.args[0];
+
   while (true) {
-    const list = discoverHosts(ns).map(host => hostInfo(ns, host));
+    const list = (() => {
+      const hosts = discoverHosts(ns)
+        .map(host => hostInfo(ns, host))
+        .filter(host => host.mm > 0)
+        .sort((a, b) => b.mm - a.mm);
+      if (maxHosts > 0) {
+        return hosts.slice(0, maxHosts);
+      }
+
+      return hosts;
+    })();
+
     ns.clearLog();
     ns.print(new Date());
     printObjList(list, ns.print);
