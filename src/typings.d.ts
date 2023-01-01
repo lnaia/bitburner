b50a087b08fa9dd6b58dbcc9907eb78bbc3fbbf2
Luis Naia <lnaia@users.noreply.github.com>
2023-01-01 01:33:29 +0000

    lock ts

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,14 +1,14 @@
 export interface HostDetails {
-    mm: number;
-    hmm: string;
-    cm: number;
-    hcm: string;
-    host: string;
-    '% diff': string;
-    rh: number;
-    ms: number;
-    cs: number;
-    ht: number;
+  mm: number;
+  hmm: string;
+  cm: number;
+  hcm: string;
+  host: string;
+  '% diff': string;
+  rh: number;
+  ms: number;
+  cs: number;
+  ht: number;
 }
 
 export type StatusReport = [boolean, string?];
