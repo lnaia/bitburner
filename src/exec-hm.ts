6d37926a60279bf1fcf9deba59fa51a7ae9ffda0
Luis Naia <lnaia@users.noreply.github.com>
2023-01-03 12:49:48 +0000

    Update exec-hm.ts

--- src/exec-hm.ts
+++ src/exec-hm.ts
@@ -5,7 +5,7 @@ export async function main(ns: NS) {
   const host = ns.args[0].toString();
 
   while (true) {
-    await hackManager(ns, host, true);
+    await hackManager(ns, host, false);
     await ns.sleep(1000);
   }
 }
