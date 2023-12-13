fd4236ddd02dd92187b90b28331a0c98af7bac53
Luis Naia <lnaia@users.noreply.github.com>
2023-12-13 14:45:33 +0000

    update static files

--- manual-copy/files-to-download.js
+++ manual-copy/files-to-download.js
@@ -1,12 +1,1 @@
-export const files = [
-  'constants.js',
-  'exec-coordinator-auto-root.js',
-  'exec-coordinator-hacknet.js',
-  'exec-coordinator.js',
-  'lib-auto-root-hosts.js',
-  'lib-discover-hosts.js',
-  'lib-log.js',
-  'lib-provision-script.js',
-  'lib-shop-nodes.js',
-  'lib-shop-servers.js',
-];
+export const files =  ["constants.js","exec-coordinator-auto-root.js","exec-coordinator-hacknet.js","exec-coordinator.js","hack-grow.js","hack-hack.js","hack-weaken.js","lib-auto-root-hosts.js","lib-discover-hosts.js","lib-hack.js","lib-log.js","lib-provision-script.js","lib-shop-nodes.js","lib-shop-servers.js","lib-time.js"];
